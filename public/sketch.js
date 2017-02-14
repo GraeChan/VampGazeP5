ec5662ce489332a68885c1147e90962681401772
GraeChan <gabsturr@hotmail.com>
2017-02-14 01:24:08 +0000

    Added vampire.js.

--- public/sketch.js
+++ public/sketch.js
@@ -1,15 +1,28 @@
 var canvas;
+var vampire;
+var vamp, coin, shop;
 
+function preload()
+{
+	coin = loadImage("sprites/coin.png");
+	vamp = loadAnimation("sprites/vampWalk1.png", "sprites/vampWalk2.png");
+}
 
 function setup() 
 {
 	canvas = createCanvas(window.innerWidth, window.innerHeight);
-	
+	vampire = new Vampire();
+	frameRate(60);
 }
 
 function draw() {
 	background(44,176,55);
-	
+
+	push();
+	animation(vamp, vampire.x, vampire.y);
+	vampire.move();
+	pop();
+
 	push();
 	fill(255,255,255);
 	ellipse(dotX, dotY, 15, 15);
---
+++ public/vampire.js
@@ -0,0 +1,61 @@
+function Vampire()
+{
+	this.x = window.innerWidth - 100;
+	this.y = window.innerHeight -  100;
+	this.width = 90;
+	this.height = 128;
+	this.xDir = 0;
+	this.yDir = 0;
+	this.direction = 0;
+
+	this.left = true;
+	this.right = false;
+	this.up = false;
+	this.down = false;
+
+	this.direction = function()
+	{
+		if(this.left == true)
+		{
+			this.right = true;
+		}
+		if (this.right == true)
+		{
+			this.left = true;
+		}
+	}
+
+	this.move = function()
+	{
+		if(vampire.left == true)
+		{
+			if(vampire.x > window.innerWidth/1.25)
+			{
+				vampire.x-=1;
+			}
+			else
+			{
+				vampire.left = false;
+				vampire.right = true;
+			}
+		}
+		
+		if(vampire.right == true)
+		{
+			if (vampire.x <= window.innerWidth - this.width)
+			{
+				vampire.x+=1;
+			}
+			else
+			{
+				vampire.left = true;
+				vampire.right = false;
+			}
+			
+		}
+		
+		
+	
+	}
+
+}
