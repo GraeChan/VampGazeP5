e73be74ce8c87ad35365eb0f3e32f8191a23eee1
GraeChan <gabsturr@hotmail.com>
2017-04-10 18:59:21 +0100

    added shop.js

--- public/shop.js
+++ public/shop.js
@@ -2,12 +2,16 @@ function Shop()
 {
     this.shop;
     this.shopSprite = loadImage("sprites/shop.png");
-   
+    this.x = 128;
+    this.y = innerHeight-128;
+    this.width = 256;
+    this.height = 256;
 	
     this.draw = function()
     {
-        this.shop = createSprite(128,innerHeight-128);
+        this.shop = createSprite(this.x,this.y);
 	    this.shop.addImage(this.shopSprite);
+        this.shop.depth = 0;
     }
     
 }
