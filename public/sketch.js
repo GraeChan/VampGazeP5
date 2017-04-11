42e2486c3be656cb5b2f25b00d911e92dab5ef2c
GraeChan <gabsturr@hotmail.com>
2017-04-11 23:04:16 +0100

    refactored code.

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,7 @@
 var canvas;
 var scl = 64;
-var vampire, sparkles, mouse, gaze, coin, shop;	// Classes
-var vampAnimWalk;
+var vampire, sparkles, mouse, gaze, coin, shop, castle;	// Classes
+//var vampAnimWalk;
 var previousMillis = 0;
 var previousMillisCoin = 0;
 var interval = 3000;
@@ -9,23 +9,26 @@ var interval = 3000;
 
 function preload()
 {
-	vampAnimWalk = loadAnimation("sprites/vampWalk1.png", "sprites/vampWalk2.png");
+	//vampAnimWalk = loadAnimation("sprites/vampWalk1.png", "sprites/vampWalk2.png");
 }
 
 function setup() 
 {
 	canvas = createCanvas(window.innerWidth-25, window.innerHeight-25);
 
-	vampire = new Vampire();
+	
 	sparkles = new Sparkles();
 	coin = new Coin();
+	vampire = new Vampire();
 	
 	shop = new Shop();
+	castle = new Castle();
 
 	shop.draw();
+	castle.draw();
 	sparkles.sparkles = new Group();
 	sparkles.draw();
-	coin.coins = new Group();
+	//coin.coins = new Group();
 	coin.drawUiCoin();
 	
 	mouse = new Mouse();
@@ -37,9 +40,15 @@ function setup()
 function draw() {
 	background(44,176,55);
 
+	coin.check();
+	sparkles.check();
+
 	push();
-	animation(vampAnimWalk, vampire.x, vampire.y);
 	vampire.move();
+	vampire.vampire.position.x = vampire.x;
+	vampire.vampire.position.y = vampire.y;
+	vampire.vampire.collide(shop.shop);
+	vampire.vampire.collide(castle.castle);
 	pop();
 
 	debug();
@@ -48,8 +57,14 @@ function draw() {
 	gaze.cursor();
 
 	mouse.hover();
-	
+	//mouse.eye.collide(shop.shop);
+	//mouse.eye.collide(castle.castle);
+	//vampAnimWalk.collide(shop.shop);
+  	//vampAnimWalk.collide(castle.castle);
+
 	drawSprites();
+
+	
 	
 	ui();
 }
@@ -75,12 +90,14 @@ function debug()
 	fill(255, 255, 0);
 	textSize(32);
 	text("      : " + coin.coinTotal, 15, 35);
-	text("EyePos: " + dotX + "," + dotY, 15, 75);
+	/*text("EyePos: " + dotX + "," + dotY, 15, 75);
 	text("MousePos: " + mouseX.toFixed(2) + "," + mouseY.toFixed(2), 15, 100);
 	text("FrameRate: " + frameRate().toFixed(2), 15, 125);
 	text("ms: " + millis().toFixed(2), 15, 150);
 	text("prevMsSparkle: " + previousMillis.toFixed(2), 15, 175);
 	text("prevMsCoin: " + previousMillisCoin.toFixed(2), 15, 200);
+	text("sparkles: " + sparkles.sparkles.length, 15, 225);
+	text("coins: " + coin.coins.length, 15, 250);*/
 	pop();
 
 }
